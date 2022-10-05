3f151a6659a2f67f7add187e5c3d4407dfc4ee4f
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-05 11:05:14 +0500

    Delete Exercise3.js

--- Exercise3.js
+++
@@ -1,3 +0,0 @@
-//var famousQuote="A person who never made a mistake never tried anything new.";
-var author="Albert Einstein once said,\"A person who never made a mistake never tried anything new.\"";
-console.log(author)

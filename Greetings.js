cd42d5dd9b13c473a0089f3727a2f92506a06970
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-05 12:20:25 +0500

    Create Greetings.js

---
+++ Greetings.js
@@ -0,0 +1,6 @@
+const array_Names =["Hammad","Wali","Adil","Arham","Fahim"];
+var greetings="May God bless you!"
+for(var i=0;i<array_Names.length;i++)
+{
+    console.log(array_Names[i]+", "+greetings);
+}

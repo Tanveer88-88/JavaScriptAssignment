596bd1bc6a6cfc1807b3b2c79eab95cc63da88f7
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-05 11:05:09 +0500

    Create Famous Quote 2.js

---
+++ Famous Quote 2.js
@@ -0,0 +1,3 @@
+//var famousQuote="A person who never made a mistake never tried anything new.";
+var author="Albert Einstein once said,\"A person who never made a mistake never tried anything new.\"";
+console.log(author)

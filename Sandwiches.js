2fe632eb1f540d8533a5bddda5449280d2e9712e
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-06 15:05:10 +0500

    Create Sandwiches.js

---
+++ Sandwiches.js
@@ -0,0 +1,7 @@
+function Make_sandwich(...args)
+{
+    console.log("Burger Order with.."+args+"\n");
+}
+Make_sandwich("Cheese","Onion","Condiments","Pickles");
+Make_sandwich("Bacon","Chilies","Avocado","Lettuce");
+Make_sandwich("Mushrooms","Tomatoes","Eggs","Chili");

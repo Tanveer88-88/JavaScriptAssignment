9842b5692c973f192993e5a9e83783f4aee0b6ef
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-06 12:50:24 +0500

    Create Magicians.js

---
+++ Magicians.js
@@ -0,0 +1,9 @@
+function show_magicians(magicianslist)
+{
+    magicianslist.forEach(element => {
+    console.log(element);
+  });
+}
+
+var magicians=["zarkoon","Hamoon","zakoota","Harbour"];
+show_magicians(magicians);

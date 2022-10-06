c768f08b0f10efcbadd9b42dd88a3de27a90f494
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-06 10:14:24 +0500

    Create Exercise20.js

---
+++ Exercise20.js
@@ -0,0 +1,17 @@
+var FavList=
+{
+    Mountain,
+    Rivers,
+    Countries,
+    Cities,
+}
+
+function MaintainList()
+{
+    FavList.Mountain="K2";
+    FavList.Rivers="Neehlum";
+    FavList.Countries="Pakistan";
+    FavList.Cities="Islamabad";
+}
+
+FavList=MaintainList();

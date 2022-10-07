cdaae7ed784ede91ae91ae5448c323d0e4cd2f3b
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 04:51:38 +0500

    Create Animals.js

---
+++ Animals.js
@@ -0,0 +1,10 @@
+let Animals=["cat","Dog","Rabit"]
+Animals.forEach(element => {
+    console.log(Animals[i]);
+});
+
+Animals.forEach(element => {
+    console.log(Animals[i]+"would make a great pet");
+});
+
+console.log("All of these pets are human friendly");

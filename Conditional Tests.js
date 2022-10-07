e0d212849e54925d416b9a9f008972a438655b53
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 03:15:03 +0500

    Create Conditional Tests.js

---
+++ Conditional Tests.js
@@ -0,0 +1,23 @@
+let car = 'subaru';
+let name='Tanveer';
+let weather="sunny";
+let programming="javascript";
+let future="Metaverse";
+let num="text";
+let string="15";
+let time="day";
+
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+console.log("Is name == 'Tanveer'? I predict True.")
+console.log(name == 'Tanveer');
+console.log("Is weather== 'sunny'? I predict True.")
+console.log(weather == 'Sunny');
+console.log("Is programming== 'javascript'? I predict True.")
+console.log(programming == 'javascript');
+console.log("Is num== 'text'? I predict false.")
+console.log(num != 'text');
+console.log("Is string== '15'? I predict false.")
+console.log(string != '15');
+console.log("Is string== 'day'? I predict false.")
+console.log(time != 'day');

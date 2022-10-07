05645a6e9d286543aea6dfc71145a08064073a89
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 04:41:00 +0500

    Create Ordinal Numbers.js

---
+++ Ordinal Numbers.js
@@ -0,0 +1,42 @@
+let numArray=[1,2,3,4,5,6,7,8,9];
+numArray.forEach(element => {
+    if(element==1)
+    {
+        console.log("ist");
+    }
+    if(element==2)
+    {
+        console.log("2nd");
+    }
+    if(element==3)
+    {
+        console.log("3rd");
+    }
+    if(element==4)
+    {
+        console.log("4th");
+    }
+    if(element==5)
+    {
+        console.log("5th");
+    }
+    if(element==6)
+    {
+        console.log("6th");
+    }
+
+    if(element==7)
+    {
+        console.log("7th");
+    }
+   
+    if(element==8)
+    {
+        console.log("8th");
+    }
+    if(element==9)
+    {
+        console.log("9th");
+    }
+    
+});

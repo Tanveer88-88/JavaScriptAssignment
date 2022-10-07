5ace8f2bb61f177121d28d78f3cf81a2adce90e6
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 04:34:17 +0500

    Create Hello Admin.js

---
+++ Hello Admin.js
@@ -0,0 +1,12 @@
+var mylist=["Admin","Abdul Hussain","Dilawar","Haider","Yasir"];
+for(var i=0;i<mylist.length;i++)
+{
+    if(mylist[i]=="Admin")
+    {
+        console.log(mylist[i]+",Hello Admin");
+    }
+    else{
+        console.log(mylist[i]+",Good Morning");
+    }
+   
+}

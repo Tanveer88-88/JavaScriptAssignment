99911e1c1c3d460625496a3f2f97f065e8c2c585
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 04:45:40 +0500

    Create Pizzas.js

---
+++ Pizzas.js
@@ -0,0 +1,10 @@
+let Pizza=["Peporoni","smoke","BBQ"]
+Pizza.forEach(element => {
+    console.log(element);
+});
+
+Pizza.forEach(element => {
+    console.log("I like" +element+"Pizza");
+});
+
+console.log("I love Pizza");

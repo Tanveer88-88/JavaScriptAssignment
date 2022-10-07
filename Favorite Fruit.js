7263c5fdb75a8bcc8d7579390e9458983383eeea
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 04:11:39 +0500

    Create Favorite Fruit.js

---
+++ Favorite Fruit.js
@@ -0,0 +1,18 @@
+var fruits=["Apple","Banana","Mango"];
+fruits.forEach(element => {
+    if(element=="Apple")
+    console.log("its Apple");
+    elseif(element=="Banana")
+    console.log("its Banana");
+    if(element=="Mango")
+    console.log("its Mango");
+});
+
+fruits.forEach(element => {
+    if(element=="Apple")
+    console.log("You really like bananas!");
+    elseif(element=="Banana")
+    console.log("You really like bananas!");
+    if(element=="Mango")
+    console.log("You really like bananas!");
+});

01103f671d26df37d9038144000b0d031040b696
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 05:14:11 +0500

    Create       Alien Colors #2.js

---
+++       Alien Colors #2.js
@@ -0,0 +1,9 @@
+let alien_color ="green";
+if(alien_color=="green")
+{
+    console.log("player just earned 5 points");
+}
+if(alien_color!="green")
+{
+ console.log("the player just earned 10 points.");
+}

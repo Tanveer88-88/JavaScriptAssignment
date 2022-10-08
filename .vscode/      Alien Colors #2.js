4aaa0f16dadd6f9e98c3b021bd0b9a8e4e11dcc2
Tanveer88-88 <sitz_tani@hotmail.com>
2022-10-08 05:14:50 +0500

    Delete       Alien Colors #2.js

--- .vscode/      Alien Colors #2.js
+++
@@ -1,9 +0,0 @@
-let alien_color ="green";
-if(alien_color=="green")
-{
-    console.log("player just earned 5 points");
-}
-if(alien_color!="green")
-{
- console.log("the player just earned 10 points.");
-}
